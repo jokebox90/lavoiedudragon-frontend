d1e1044c4d6501b204b8d19a5b05534a42ef48a8
Jokebox90 <dev.jvagnier@gmail.com>
2023-07-25 17:05:45 +0200

    Adapte les couleurs et ajoute ds polices

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,30 +5,36 @@ module.exports = {
     themes: [
       {
         owner: {
-          primary: "#1C05AD",
-
+          primary: "#130477",
           secondary: "#AC66F2",
-
           accent: "#A3F26B",
-
           neutral: "#aab5c0",
-
           "base-100": "#FAFAFA",
-
           info: "#0092d6",
-
           success: "#6cb288",
-
           warning: "#daad58",
-
           error: "#ab3d30",
         },
       },
     ],
   },
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: "Racing Sans One",
+        sans: "Noto Sans",
+        serif: "Noto Serif",
+        ubuntu: "Ubuntu",
+        roboto: "Roboto",
+        titilium: "Titillium Web",
+        source: "Source Sans Pro",
+        "source-code": "Source Code Pro",
+        bree: "Bree Serif",
+        noto: "Noto Sans",
+        "noto-serif": "Noto Serif",
+        racing: "Racing Sans One",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
-
