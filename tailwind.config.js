228dd00746b97db74472dc0245bff69bf75c7d13
Jokebox90 <dev.jvagnier@gmail.com>
2023-07-25 15:05:38 +0200

    Installe DaisyUI et configure un thème de couleur

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        owner: {
+          primary: "#1C05AD",
+
+          secondary: "#AC66F2",
+
+          accent: "#A3F26B",
+
+          neutral: "#aab5c0",
+
+          "base-100": "#FAFAFA",
+
+          info: "#0092d6",
+
+          success: "#6cb288",
+
+          warning: "#daad58",
+
+          error: "#ab3d30",
+        },
+      },
+    ],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
 
